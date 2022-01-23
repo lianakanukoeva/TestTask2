88b811ff1402c8c611215b70938624c697645b96
lianakanukoeva <lianakanukoeva@yandex.ru>
2022-01-23 19:45:42 +0300

    created new project

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './css/style.scss';
+import './js/new';
+console.log("hello");
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+
+module.exports = {
+    mode: mode,
+    output: {
+        assetModuleFilename: "assets/[hash][ext][query]",
+        clean: true
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [{
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jped|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+}
