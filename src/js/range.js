90eac8a880dbc17d2c5c413e5ff047fa6c04503e
lianakanukoeva <lianakanukoeva@yandex.ru>
2022-02-01 03:35:36 +0300

    edit paddings & js

--- src/js/range.js
+++ src/js/range.js
@@ -5,3 +5,11 @@ progress.addEventListener('input', function() {
     this.style.background = `linear-gradient(to right, #FEE600 0%, #FEE600 ${value}%, #E1E1E3 ${value}%, #E1E1E3 100%)`
     console.log('right')
 })
+
+const progress_1 = document.querySelector('.second');
+
+progress_1.addEventListener('input', function() {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, #FEE600 0%, #FEE600 ${value}%, #E1E1E3 ${value}%, #E1E1E3 100%)`
+    console.log('right')
+})
